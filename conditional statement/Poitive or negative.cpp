09f05a0a4d9863c7d6b5e5fa9bc463f7953aae6d
PratikM21 <106132530+PratikM21@users.noreply.github.com>
2022-06-22 23:03:51 +0600

    Add files via upload

---
+++ conditional statement/Poitive or negative.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+	int a;
+	printf("Enter the number:");
+	scanf("%d",&a);
+	if(a>=0){
+		printf("The number is positive");
+	}
+	else{
+		printf("The number is negative");
+	}
+	
+	
+	
+	
+	
+	return 0;
+}
---
+++ conditional statement/Two numbers equal or not.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+	int a,b;
+	printf("Enter the first value:");
+	scanf("%d",&a);
+	printf("Ebter the second value:");
+	scanf("%d",&b);
+	
+	if (a==b){
+		printf("The numbers are equal");
+	}
+	else{
+		printf("The numbers are not equal");
+	}
+	
+	
+	
+	return 0;
+}
